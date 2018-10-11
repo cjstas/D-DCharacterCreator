81bb4d2a57655f29f562f874b161030c5800c902
CaKellum <42648546+CaKellum@users.noreply.github.com>
2018-10-11 17:40:27 -0400

    UICommit

---
+++ src/main/java/edu/bsu/cs222/finalproject/veiw/CharacterSheets.java
@@ -0,0 +1,27 @@
+package edu.bsu.cs222.finalproject.veiw;
+
+import javafx.scene.layout.*;
+
+public class CharacterSheets {
+
+    public static Pane setSheet(String sheet) {
+        if(sheet.equals("3.5")){
+            return sheet35();
+        }else{
+            return sheet5e();
+        }
+    }
+
+    private static Pane sheet35() {
+        Pane sheet35 = new BorderPane();
+        BorderPane stats = new BorderPane();
+
+
+        return sheet35;
+    }
+
+    private static Pane sheet5e() {
+
+        return null;
+    }
+}
