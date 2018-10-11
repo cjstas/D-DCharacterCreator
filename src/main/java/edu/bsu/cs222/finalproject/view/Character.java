ec084cbc4d65f134cade5096a2a60037a03d823c
Aron Patterson <42646205+aronpatterson2@users.noreply.github.com>
2018-10-11 18:13:53 -0400

    commit

--- src/main/java/edu/bsu/cs222/finalproject/view/Character.java
+++ src/main/java/edu/bsu/cs222/finalproject/view/Character.java
@@ -10,18 +10,27 @@ public class Character {
     private int wis;
     private int cha;
     private String name;
+    private String race;
+
+    public void Character(){
+
+    }
 
     public void CharacterName(String name){
         this.name=name;
     }
 
-    public void stats (int str, int dex, int con, int intel, int wis, int cha){
+    public void Stats (int str, int dex, int con, int intel, int wis, int cha){
         this.str=str;
         this.dex=dex;
         this.con=con;
         this.intel=intel;
         this.wis=wis;
         this.cha=cha;
+    }
+
+    public void Race(String race){
+        this.race=race;
     }
 
 
