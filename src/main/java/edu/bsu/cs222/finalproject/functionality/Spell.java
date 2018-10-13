4de0a1e2469a000009d9addbfdf97f226fbddf40
Christopher Staszewski <c.stasz@outlook.com>
2018-10-13 14:40:16 -0400

    +DiceRoll.java has been Tested with DiceRollTests.java +Spell.java has been made     +String name     +private ArrayList<String> usableBy     +int spellslot     +public void isUseableBy(String user) +SpellTests     +Added a large amount of tests for 2 spells with diffrent classes     ~Changed original plan so that spellslot is public and can be compared on the fly and not within the class (might want to change back, but probably not) ~~~Changed jdk version to 10 so that we are consistent and hopefully avoid problems with workspace.xml ~~~Cleaned code so that it worked better and looked nicer

--- src/main/java/edu/bsu/cs222/finalproject/functionality/Spell.java
+++ src/main/java/edu/bsu/cs222/finalproject/functionality/Spell.java
@@ -1,4 +1,26 @@
 package edu.bsu.cs222.finalproject.functionality;
 
+import java.util.ArrayList;
+
 public class Spell {
+    public String name;
+    private ArrayList<String> useableBy;
+    public int spellSlotLevel;
+
+    Spell(String name,ArrayList<String> useableBy, int spellSlotLevel) {
+        this.name = name;
+        this.useableBy = useableBy;
+        this.spellSlotLevel = spellSlotLevel;
+    }
+
+
+    public boolean isCastableBy(String user) {
+        boolean result = false;
+        for(String users : useableBy) {
+            if(user.equals(users)) {
+                result = true;
+            }
+        }
+        return result;
+    }
 }
